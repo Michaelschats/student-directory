66d3c40efd097a08262dd340d11d4fd1c53aa0e5
Michael Schats <michael_schats@hotmail.com>
2020-10-21 17:22:54 +0100

    Exercise 2 Completed

--- directory2.rb
+++ directory2.rb
@@ -23,8 +23,10 @@ def print_header
 end
 
 def print(students)
-  students.each.with_index(1) do |student,index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    if student.start_with?('s')
+    puts "#{student.capitalize}"
+    end
   end
 end
 
