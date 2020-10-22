971d6dfaafa7b0f867f9c727cde92fdb1f809abb
Michael Schats <michael_schats@hotmail.com>
2020-10-22 12:07:32 +0100

    Exercise 5 Completed

--- directory2.rb
+++ directory2.rb
@@ -1,17 +1,21 @@
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "Please enter the names, hobby and birth place of each student"
+  puts "To finish, hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
+  hobbies = gets.chomp
+  birth_place = gets.chomp
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? && !hobbies.empty? && !birth_place.empty?
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: :november, hobbies: hobbies, birth_place: birth_place}
     puts "Now we have #{students.count} students"
     # get another name from user
     name = gets.chomp
+    hobbies = gets.chomp
+    birth_place = gets.chomp
   end
   # return array of input_students
   students
@@ -23,16 +27,8 @@ def print_header
 end
 
 def print(students)
-  copy_of_students = students.map(&:clone)
-  student_number = 1
-  while !copy_of_students.empty?
-    if copy_of_students[0][:name].length < 12
-      puts "#{student_number}. #{copy_of_students[0][:name]} (#{copy_of_students[0][:cohort]} cohort)"
-      student_number += 1
-      copy_of_students.delete_at(0)
-    else
-      copy_of_students.delete_at(0)
-    end
+  students.each do |student|
+    puts "#{student[:name]}, (#{student[:cohort]} cohort, #{student[:hobbies]}, #{student[:birth_place]})"
   end
 end
 
